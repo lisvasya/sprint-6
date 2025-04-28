2a014af77721f617ab54fcf73b2436d12ad85a88
Vasilisa Mahonina <Vasilisamahonina62@yandex.ru>
2025-04-28 15:27:52 +0300

    ninthh commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -92,6 +92,6 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Отправляем результат конвертации пользователю
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	_, _ = w.Write([]byte(convertedStr))
 }
